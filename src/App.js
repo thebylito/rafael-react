67b5c53779d1e01b8665be366d9de88d143c2042
Welington da Silva <thebylito@gmail.com>
2019-05-29 19:45:02 -0400

    explicacao React component e stateless

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Stateless
+class MostraDados extends React.Component {
+  state = {
+    logado: true,
+  };
+  componentDidMount() {
+    console.log('entrei');
+  }
+
+  componentWillUnmount() {
+    console.log('sai');
+  }
+
+  componentWillReceiveProps() {
+    console.log('recebi um atualizacao');
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div
+        style={{
+          color: 'green',
+        }}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    nome: 'Welington',
+    sobrenome: 'Silva',
+    exibindo: true,
+  };
+
+  componentDidMount() {
+    this.alterarDados();
+  }
+
+  alterarDados = () => {
+    setTimeout(() => {
+      this.setState({
+        nome: 'Caio',
+        sobrenome: 'Henrique',
+      });
+    }, 3000);
+  };
+
+  componentWillUnmount() {}
+
+  escondeDados = () => {
+    this.setState({
+      exibindo: false,
+    });
+  };
+
+  render() {
+    const { nome, sobrenome, exibindo } = this.state;
+    return (
+      <div>
+        {exibindo === true ? (
+          <>
+            <MostraDados>{nome}</MostraDados>
+            <MostraDados>{sobrenome}</MostraDados>
+          </>
+        ) : null}
+
+        <button onClick={this.escondeDados}>Esconde Dados</button>
+      </div>
+    );
+  }
 }
 
 export default App;
